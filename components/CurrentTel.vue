5d69e33614f12559d4cdf025179bc474c4bcc2f6
shurijoc <re.e.poch89@gmail.com>
2022-07-02 15:08:07 +0900

    :+1: add fontawesome

--- components/CurrentTel.vue
+++ components/CurrentTel.vue
@@ -2,14 +2,38 @@
   <div class="content is-flex is-align-items-center	is-justify-content-center is-flex-flow-column">
     <p class="is-size-5 has-text-grey">現在の荷電状況</p>
     <p class="is-size-3">
-      <i class="fas fa-home"></i>
-      080-0000-0000
+      <i class="fa-solid fa-phone" :class="phoneIconClass"></i>
+      {{ phoneNumber }}
     </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CurrentTel'
+  name: 'CurrentTel',
+  data () {
+    return {
+      isCalling: false,
+      phoneNumber: "080-0000-0000"
+    }
+  },
+  computed: {
+    phoneIconClass() {
+      if (this.isCalling) {
+        return 'fa-shake'
+      } else {
+        return ''
+      }
+    },
+  },
+  methods: {
+    changeIsCalling() {
+      this.isCalling = !this.isCalling
+      return
+    }
+  },
+  created () {
+    setInterval(this.changeIsCalling, 3000);
+  },
 }
 </script>
